e2c4f49dbbfb50cf1defa6a27bb5513726d2278a
C18324463 <c18324463@mytudublin.ie>
2022-01-26 13:04:03 +0000

    trying to get sidebar working

--- fyp/src/App.js
+++ fyp/src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
+import React, { useRef } from "react";
+
 
 function App() {
+  function openNav() {
+    var name = React.findDOMNode(this.refs.cpDev1).value;
+    this.someOtherFunction(name);
+    this.myRef= React.createRef();
+
+    //document.getElementById("mySidenav").style.width = "250px";
+    //document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    //document.getElementById("mySidenav").style.width = "0";
+    //document.getElementById("main").style.marginLeft= "0";
+  }
+
   return (
     <div>
       {/*Comments*/}
-      <div id="display">
+      <div id="mySidenav" class="sidenav">
+        <a href="javascript:void(0)" class="closebtn" onClick={() => closeNav()}>&times;</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Clients</a>
+        <a href="#">Contact</a>
+      </div>
+      <div id="main" ref={this.myRef}>
+        <span style="font-size:30px;cursor:pointer" onClick={() => openNav()}>&#9776; Open</span>
       </div>
     </div>
   );
