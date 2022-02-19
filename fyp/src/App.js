19e75a7eb0d779daced5b497ab3ebd13dc219a11
C18324463 <c18324463@mytudublin.ie>
2022-02-19 00:00:40 +0000

    SideNav Bar created

--- fyp/src/App.js
+++ fyp/src/App.js
@@ -1,35 +1,21 @@
-import './App.css';
-import React, { useRef } from "react";
+import "./App.css";
+import React, {useState} from "react";
+import SideNav from "./SideNav";
 
 
-function App() {
-  function openNav() {
-    var name = React.findDOMNode(this.refs.cpDev1).value;
-    this.someOtherFunction(name);
-    this.myRef= React.createRef();
-
-    //document.getElementById("mySidenav").style.width = "250px";
-    //document.getElementById("main").style.marginLeft = "250px";
+function App(props, state) {
+  const [wid, setWid] = useState('0%');
+  const openSidenav = ( ) => {
+    setWid('25%')
   }
-  
-  function closeNav() {
-    //document.getElementById("mySidenav").style.width = "0";
-    //document.getElementById("main").style.marginLeft= "0";
+  const closeSidenav = ( ) => {
+    setWid('0%')
   }
 
   return (
-    <div>
-      {/*Comments*/}
-      <div id="mySidenav" class="sidenav">
-        <a href="javascript:void(0)" class="closebtn" onClick={() => closeNav()}>&times;</a>
-        <a href="#">About</a>
-        <a href="#">Services</a>
-        <a href="#">Clients</a>
-        <a href="#">Contact</a>
-      </div>
-      <div id="main" ref={this.myRef}>
-        <span style="font-size:30px;cursor:pointer" onClick={() => openNav()}>&#9776; Open</span>
-      </div>
+    <div className="container">
+      <button onClick={openSidenav}>Open</button>
+      <SideNav name="Jonathan" width={wid} closeNav={closeSidenav}/>
     </div>
   );
 }
