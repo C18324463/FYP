cc2a1445f3bd6962dc88138f761c2a865bc3f339
C18324463 <c18324463@mytudublin.ie>
2022-02-21 10:51:17 +0000

    Added logo

--- fyp/src/SideNav.js
+++ fyp/src/SideNav.js
@@ -1,9 +1,11 @@
 import React from "react";
+import logo from "./img/Untitled.png"
 
 const SideNav = (props) => {
    return (
       <div className="sidenav" style={{ width: props.width, paddingTop: '20px' }}>
          <button className="closebtn" onClick={props.closeNav}>X</button>
+         <img src={logo}/>
          <a href="#section">Home</a>
          <a href="#section">F1 Live</a>
          <a href="#section">Standings</a>
