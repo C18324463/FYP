9e3598c8c49217cf3e60494f07c97077b56e5021
C18324463 <c18324463@mytudublin.ie>
2022-02-22 22:23:36 +0000

    searchbar added

--- fyp/src/SideNav.js
+++ fyp/src/SideNav.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useState} from 'react';
+import TextField from "@mui/material/TextField";
 import logo from "./img/Untitled.png"
 
 const SideNav = (props) => {
+   const [inputText, setInputText] = useState("");
+   let inputHandler = (e) => {
+     var lowerCase = e.target.value.toLowerCase();
+     setInputText(lowerCase);
+   };
+
    return (
       <div className="sidenav" style={{ width: props.width, paddingTop: '20px' }}>
          <button className="closebtn" onClick={props.closeNav}>X</button>
          <img src={logo}/>
+         <div className="search">
+            <TextField id="outlined-basic" variant="outlined" fullWidth label="Search"/>
+         </div>
+         {/*<List input={inputText} />*/}
          <a href="#section">Home</a>
          <a href="#section">F1 Live</a>
          <a href="#section">Standings</a>
