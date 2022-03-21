7c594ee1d148ac6e2e7869b32fa72d543f261bf3
C18324463 <c18324463@mytudublin.ie>
2022-03-21 21:29:17 +0000

    Standings CSS added

--- fyp/src/Home.js
+++ fyp/src/Home.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React from 'react';
-import SideNav from './SideNav';
 
 export default function Home(){
     return(
--- fyp/src/Standings.js
+++ fyp/src/Standings.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './Standings.css';
 import React, {useEffect, useState} from 'react';
 
 
@@ -25,27 +25,46 @@ function Standings(){
 
   return(
     <div>
-      <table>
-          {results.MRData?.StandingsTable?.StandingsLists[0].DriverStandings.map(element => {
-            return (
-              <p key={element.Driver.driverId}>
-              {element.position}
-              {" "}
-              {element.Driver.givenName}
-              {" "}
-              {element.Driver.familyName}
-              {" "}
-              {element.Driver.permanentNumber}
-              {" "}
-              {element.Constructors[0].name}
-              {" "}
-              {element.points}
-              </p>
-            )
-          }  )}
+      <br></br>
+      <table id='table_standings' >
+        <thead>
+          <tr>
+            <th>Position</th>
+            <th>Driver</th>
+            <th>Number</th>
+            <th>Team</th>
+            <th>Points</th>
+          </tr>
+        </thead>
+        <tbody className='trtable'>
+            {results.MRData?.StandingsTable?.StandingsLists[0].DriverStandings.map(element => {
+              return (
+                <tr>
+                  <td key={element.Driver.driverId} className='trtable'>
+                    {element.position}
+                  </td>
+                  <td key={element.Driver.dateOfBirth}>
+                    {element.Driver.givenName}
+                    {" "}
+                    {element.Driver.familyName}
+                  </td>
+                  <td key={element.Driver.permanentNumber}>
+                    {element.Driver.permanentNumber}
+                  </td>
+                  <td key={element.positionText}>
+                    {element.Constructors[0].name}
+                  </td>
+                  <td key={element.Driver.code}>
+                    {element.points}
+                  </td>
+                </tr>
+              )
+            }  )}
+        </tbody>
       </table>
     </div>
   )
 }
 
 export default Standings;
+
