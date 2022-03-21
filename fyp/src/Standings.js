04ac29cbb8377302a7f212450774127ec56d79b6
C18324463 <c18324463@mytudublin.ie>
2022-03-21 17:36:03 +0000

    Update Standings.js
    Data displaying from API on the page

--- fyp/src/Standings.js
+++ fyp/src/Standings.js
@@ -1,53 +1,49 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 
 function Standings(){
-  let x = {};
-  let y = false;
-  const [results, setResults] = useState();
+  const [results, setResults] = useState([]);
   var requestOptions = {
     method: 'GET',
     redirect: 'follow'
   };
 
-  if(x === null) {
-    y = true;
-  } else {
-    initProducts();
-  }
-
-  async function initProducts() {
-    await fetch(`http://ergast.com/api/f1/2022/1/driverStandings.json`, requestOptions)
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch(`http://ergast.com/api/f1/2022/1/driverStandings.json`, requestOptions)
       .then(response => response.text())
       .then(response2 => JSON.parse(response2))
-      .then(result => x = result)
       .then(result => setResults(result))
       .catch(error => console.log('error', error));
-  }
+    };
+    fetchData();
+  }, []);
 
-  /*help(result)
-  function help(result) {
-    setResults(result);
-    x = result;
-  }*/
-
-  console.log(x);
   console.log(results);
-  console.log("WHY!");
-  JSON.parse(JSON.stringify(x))
-  x = JSON.stringify(x);
+  console.log(results.MRData?.StandingsTable?.StandingsLists[0].DriverStandings[0]);
 
   return(
     <div>
-      <button>Click</button>
-      <div>
-          {x?.MRData?.map(element => {
-            <h1>
-              {element}
-            </h1>
-          } )}
-      </div>
+      <table>
+          {results.MRData?.StandingsTable?.StandingsLists[0].DriverStandings.map(element => {
+            return (
+              <p key={element.Driver.driverId}>
+              {element.position}
+              {" "}
+              {element.Driver.givenName}
+              {" "}
+              {element.Driver.familyName}
+              {" "}
+              {element.Driver.permanentNumber}
+              {" "}
+              {element.Constructors[0].name}
+              {" "}
+              {element.points}
+              </p>
+            )
+          }  )}
+      </table>
     </div>
   )
 }
