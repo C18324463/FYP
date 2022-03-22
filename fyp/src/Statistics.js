d1ab2317c879ac22c651fe0bcf0756789cfeb13c
C18324463 <c18324463@mytudublin.ie>
2022-03-22 18:22:37 +0000

    Cards with Images added
    Statistics page now displays cards for drivers with images of drivers

--- fyp/src/Statistics.js
+++ fyp/src/Statistics.js
@@ -1,10 +1,64 @@
-import './App.css';
-import React from 'react';
+import './Statistics.css';
+import React, {useEffect, useState} from 'react';
+import {Card} from 'react-bootstrap';
+import driver_images from "./img/driver_images";
 
-export default function Statistics(){
+function Statistics(){
+    const [info, setInfo] = useState([]);
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        await fetch(`http://ergast.com/api/f1/2022/drivers.json`, requestOptions)
+        .then(response => response.text())
+        .then(response2 => JSON.parse(response2))
+        .then(result => setInfo(result))
+        .catch(error => console.log('error', error));
+      };
+      fetchData();
+    }, []);
+
+    console.log(info);
+    console.log(info.MRData?.DriverTable.Drivers);
+    console.log(driver_images);
+
+    let counter = -1;
+
     return(
         <div>
-          
+            <button id='info_constructors'>Constructors</button>
+            <br></br>
+            <br></br>
+            <button id='info_tracks'>Tracks</button>
+            <div>
+            {info.MRData?.DriverTable?.Drivers.map(element => {
+                counter = counter + 1;
+              return (
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img className='img' variant="top" src={driver_images[counter]}/>
+                    <Card.Body>
+                        <Card.Title key={element.driverId}>
+                            {element.givenName}
+                            {" "}
+                            {element.familyName}
+                        </Card.Title>
+                        <Card.Text>
+                            {element.dateOfBirth}
+                            <br></br>
+                            {element.nationality}
+                            <br></br>
+                            {element.permanentNumber}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+              )
+            }  )}
+            </div>
         </div>
     )
 }
+
+export default Statistics;
---
+++ fyp/src/img/driver_images.js
@@ -0,0 +1,28 @@
+import React, {useEffect, useState} from 'react';
+import Albon from "./albon.jpg";
+import Alonso from "./alonso.jpg";
+import Bottas from "./bottas.jpg";
+import Gasly from "./gasly.jpg";
+import Hamilton from "./hamilton.jpg";
+import Hulkenburg from "./hulkenburg.jpg";
+import Magnussen from "./kevin_magnussen.jpg";
+import Latifi from "./latifi.jpg";
+import Leclerc from "./leclerc.jpg";
+import Verstappen from "./max_verstappen.jpg";
+import Schumacher from "./mick_schumacher.jpg";
+import Norris from "./norris.jpg";
+import Ocon from "./ocon.jpg";
+import Perez from "./perez.jpg";
+import Ricciardo from "./ricciardo.jpg";
+import Russell from "./russell.jpg";
+import Sainz from "./sainz.jpg";
+import Stroll from "./stroll.jpg";
+import Tsunoda from "./tsunoda.jpg";
+//import Vettel from "./vettel.jpg";
+import Zhou from "./zhou.jpg";
+
+
+const driver_images = [Albon, Alonso, Bottas, Gasly, Hamilton, Hulkenburg, Latifi, Leclerc, Magnussen,
+    Norris, Ocon, Perez, Ricciardo, Russell, Sainz, Schumacher, Stroll, Tsunoda, Verstappen, Zhou];
+
+export default driver_images;
